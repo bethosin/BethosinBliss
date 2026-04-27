4c35e5644ed1bb076b5cde6028122ba517c093e4
Bethosin <lizzytod@gmail.com>
2026-04-27 15:39:56 +0100

    commit

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,7 +2,7 @@
 export const WHATSAPP_NUMBER = "2348107851093";
 
 // Display phone number (formatted for users)
-export const PHONE_NUMBER = "+234 8107 841093";
+export const PHONE_NUMBER = "+234 8107 851093";
 
 // Business location
 export const LOCATION = "Ibadan, Nigeria";
